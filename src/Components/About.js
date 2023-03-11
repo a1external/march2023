c386492efaddd1e99b9b4469aee8c899e09a58e0
mujteba Khan <mujteba.a1external.com>
2023-03-11 04:40:04 -0800

    selflearning change in About

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,7 @@
 import {useState, useEffect} from "react"
 import {Link} from "react-router-dom"
 import axios from "axios"
+// this about page
 const About = () => {
 const [temp, settemp] = useState([])
 const Apilink ="https://fakestoreapi.com/products"
